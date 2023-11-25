6005188f57fdcd23c4fc6cd634661c8d8a9561ba
RGSW Digital Genius <151096334+RGSWDigitalGenius@users.noreply.github.com>
2023-11-25 10:16:18 +0000

    Update JS file for index page

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,21 @@
-var i = 0;
-var txt = 'RGSW Digital Genius : Site Version 0.2';
-var speed = 20;
+// var i = 0;
+// var txt = 'RGSW Digital Genius : Site Version 0.2';
+// var speed = 20;
+// 
+// function run() {
+//     if (i < txt.length) {
+//         document.getElementById("text").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(type, speed);
+//     }
+// }
 
-function type() {
-    if (i < txt.length) {
-        document.getElementById("text").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(type, speed);
-    }
+function openForm() {
+    document.getElementById("updatePopup").style.display = "block";
 }
-
-window.onload = type;
+  
+function closeForm() {
+    document.getElementById("updatePopup").style.display = "none";
+}
+  
+window.onload = openForm;
